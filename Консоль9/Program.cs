4ef11055e4cbb8305f50dec2e28aa662d9b4c1b8
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-09-23 15:47:53 +0300

    Add files via upload

---
+++ Консоль9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Консоль9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длинну каких слов вывести из файла?");
+            int lihg = Convert.ToInt32(Console.ReadLine());
+            string file = File.ReadAllText(@"C:\Users\egora\Desktop\Текст.txt");
+            file = file.Replace(".", "");
+            string[] person = file.Split(' ');
+            for (int i = 0; i < person.Length; i++)
+            {
+                if(person[i].Length==lihg)
+                {
+                    Console.WriteLine(person[i]);
+                }
+            }
+            //////////////////////////ЗАДАНИЕ9.2/////////////////////////////
+            var SW = new StreamWriter(@"C:\Users\egora\Desktop\Файл записи.txt", true);
+            for (int i = 0; i < person.Length; i++)
+            {
+                if (person[i].Length > lihg)
+                {
+                    SW.WriteLine(person[i]);
+                    
+                }
+            }
+            SW.Close();
+            Console.ReadKey();
+        }
+    }
+}
